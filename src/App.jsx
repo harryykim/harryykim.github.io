5a8da844b22c50f27aa1bf0f71b50ae9cb4d00e4
Harry Kim <harry.yunhoo.kim@gmail.com>
2025-04-21 00:14:14 -0700

    Added basic Home section with brief description of myself.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar } from './components/NavBar'
+import { Home } from './components/sections/Home'
 
 function App() {
   return (
     <>
       <div className={`min-h-screen bg-black text-gray-100`}>
           <Navbar />
+          <Home />
         </div>
     </>
   )
