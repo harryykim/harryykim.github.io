f1b4d2331837a8f2fba2017bd5553e0adfcbba6a
Harry Kim <harry.yunhoo.kim@gmail.com>
2025-04-22 14:45:28 -0700

    Added contact links and another project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -8,7 +8,9 @@ export const Projects = () => {
             <h1 className="text-3xl font-bold mb-8 text-cyan-500 text-center">Projects</h1>
             <div className="p-6 border rounded-border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59,130,2246,0.2)]">
                 <h3>Rollcall</h3>
-                <img src="/images/RollCall%20screenshot.png" alt="A screenshot of the RollCall application." class="w-128 h-auto rounded-lg shadow-md" />
+                <div className="flex justify-center items-center">
+                    <img src="/images/RollCall%20screenshot.png" alt="A screenshot of the RollCall application." className="w-128 h-auto rounded-lg shadow-md" />
+                </div>
                 <p className="text-gray-400 mb-4">
                     An attendance management application for after school programs.
                 </p>
@@ -24,6 +26,31 @@ export const Projects = () => {
                 </div>
                 <div className="flex justify-between items-center">
                     <a href="https://final-project-20-waves.vercel.app/login"
+                    className="text-blue-400 hover:text-blue-300 transition-colors">
+                        Project Link →
+                    </a>
+                </div>
+            </div>
+            <div className="p-6 border rounded-border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59,130,2246,0.2)]">
+                <h3>Cybercision</h3>
+                <div className="flex justify-center items-center">
+                    <img src="/images/cybercision.png" alt="A screenshot of the Cybercision game." className="w-128 h-auto rounded-lg shadow-md" />
+                </div>
+                <p className="text-gray-400 mb-4">
+                    Cybercision, a puzzle simulation game made by Team 9, UBC Game Dev 2022-2023
+                </p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                    {["Unity", "Game Development"].map((tech,key) =>(
+                        <span 
+                            key={key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                                   hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                            {tech}
+                        </span>
+                    ))}
+                </div>
+                <div className="flex justify-between items-center">
+                    <a href="https://engineereng.itch.io/cybercision"
                     className="text-blue-400 hover:text-blue-300 transition-colors">
                         Project Link →
                     </a>
