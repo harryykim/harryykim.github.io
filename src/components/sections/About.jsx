ba2912d2a76d43514c34a86af49a6e7068a42e00
Harry Kim <harry.yunhoo.kim@gmail.com>
2025-04-21 22:50:54 -0700

    Added some dummy text to about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,5 +1,12 @@
 
 export const About = () => {
-    return <section id="about">
+    return <section id="about"
+    className="min-h-screen flex items-center justify-center">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-3xl font-bold mb-20 text-cyan-500">About Me</h1>
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                Dummy text
+            </p>
+        </div>
     </section>
 }
