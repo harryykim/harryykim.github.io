6929f45eaae3ff9775f54d405ebf319d6685a83f
Harry Kim <harry.yunhoo.kim@gmail.com>
2025-04-21 22:52:09 -0700

    updated about text

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,9 +3,11 @@ export const About = () => {
     return <section id="about"
     className="min-h-screen flex items-center justify-center">
         <div className="text-center z-10 px-4">
-            <h1 className="text-3xl font-bold mb-20 text-cyan-500">About Me</h1>
+            <h1 className="text-3xl font-bold mb-8 text-cyan-500">About Me</h1>
             <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
-                Dummy text
+                I am a hardworking, intellectual individual studying Software Systems at Simon Fraser University. I'm interested in software development, and have experience in languages like C++ and Java, and am learning some web design and React.
+                As I continue my journey in software development, I'm excited to explore new technologies and their real-world applications.
+                When I'm not coding, I enjoy playing video games and working on creative projects.
             </p>
         </div>
     </section>
